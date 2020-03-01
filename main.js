e170e80a488c673f0da9bbf0a56b97df59767785
hkKevin <pakkiuleung@gmail.com>
2020-03-01 13:15:51 +0800

    Revamped the login form with animations and images

---
+++ main.js
@@ -0,0 +1,20 @@
+const inputs = document.querySelectorAll('input');
+
+function focusFunc() {
+  // Add a class to the grand-parent's node
+  let parent = this.parentNode.parentNode;
+  parent.classList.add('focus');
+}
+
+function blurFunc() {
+  let parent = this.parentNode.parentNode;
+  // if the value of input field is null...
+  if ( this.value == '' ) {
+    parent.classList.remove('focus');
+  }
+}
+
+inputs.forEach(input => {
+  input.addEventListener('focus', focusFunc);
+  input.addEventListener('blur', blurFunc);
+});
